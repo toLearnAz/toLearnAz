9ca31f8184ef4a7c8de9c9176db0a536aec2c77a
toLearnAz <114563126+toLearnAz@users.noreply.github.com>
2023-05-23 12:51:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -32,26 +32,6 @@ div.desc {
   <div class="desc">Add a description of the image here</div>
 </div>
 
-<div class="gallery">
-  <a target="_blank" href="img_forest.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS2.jpg" alt="Forest" width="600" height="400">
-  </a>
-  <div class="desc">Add a description of the image here</div>
-</div>
-
-<div class="gallery">
-  <a target="_blank" href="img_lights.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS3.jpg" alt="Northern Lights" width="600" height="400">
-  </a>
-  <div class="desc">Add a description of the image here</div>
-</div>
-
-<div class="gallery">
-  <a target="_blank" href="img_mountains.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS4.jpg" alt="Mountains" width="600" height="400">
-  </a>
-  <div class="desc">Add a description of the image here</div>
-</div>
 
 </body>
 </html> 
