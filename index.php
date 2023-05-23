22383386354852519154317ccfb7ede466a6c262
toLearnAz <114563126+toLearnAz@users.noreply.github.com>
2023-05-23 12:16:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,5 @@
 </head>
 <body>
 <?php echo "Hello world"; ?>
- <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS1.jpg" alt="its a boy! " width="500" height="600"> 
 </body>
 </html>
