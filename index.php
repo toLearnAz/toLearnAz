0f501da74bc7c3754a99acfa8c0c51b8e2da6924
toLearnAz <114563126+toLearnAz@users.noreply.github.com>
2023-05-23 11:59:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,6 @@
 <title>Hello World</title>
 </head>
 <body>
-   
-<?php
-class IndexController {
-   /* default action */
-   public function indexAction() {
-       $this->_view->word = "hello world";
-       //or
-       // $this->getView()->word = "hello world";
-   }
-}
-?>
-<?php echo $word;?>
+
 </body>
 </html>
