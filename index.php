6f8479e859d8e93e29d10b3ce5af91cfac4010f9
toLearnAz <114563126+toLearnAz@users.noreply.github.com>
2023-05-23 12:48:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,57 @@
-<html>
+ <html>
 <head>
-<title>Hello World</title>
+<style>
+div.gallery {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+</style>
 </head>
 <body>
-<?php echo "Hello world"; ?>
+
+<div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS1.jpg" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_forest.jpg">
+    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS2.jpg" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_lights.jpg">
+    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS3.jpg" alt="Northern Lights" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_mountains.jpg">
+    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS4.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
 </body>
-</html>
+</html> 
