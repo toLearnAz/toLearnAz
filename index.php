344a84efc22c2b4fefe0f4a58c0abf27743f169b
toLearnAz <114563126+toLearnAz@users.noreply.github.com>
2023-05-23 16:18:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -25,10 +25,12 @@ div.desc {
 </head>
 <body>
   <?php 
+  $string1= "https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS"
+  $string2= ".jpg"  
   for ($i = 1; $i <= 53; $i++) {
     <div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS$i.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS$i.jpg" width="600" height="400">
+  <a target="_blank" href=$string+$i+$string2>
+    <img src=$string+$i+$string2 width="600" height="400">
   </a>
   <div class="desc">$i</div>
   ?>
