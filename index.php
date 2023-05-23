69d6c078592f6c93ae6102f9af4ddc6d563738c6
toLearnAz <114563126+toLearnAz@users.noreply.github.com>
2023-05-23 16:11:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -24,65 +24,13 @@ div.desc {
 </style>
 </head>
 <body>
-
-<div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS1.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS1.jpg" width="600" height="400">
+  <?php 
+  for ($i = 1; $i <= 53; $i++) {
+    <div class="gallery">
+  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS$i.jpg">
+    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS$i.jpg" width="600" height="400">
   </a>
-  <div class="desc">PHOTOS1</div>
-</div>
-  <div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS2.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS2.jpg"  width="600" height="400">
-  </a>
-  <div class="desc">PHOTOS2</div>
-</div>
-  <div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS3.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS3.jpg" alt="Cinque Terre" width="600" height="400">
-  </a>
-  <div class="desc">PHOTOS3</div>
-</div>
-  <div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS4.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS4.jpg" alt="Cinque Terre" width="600" height="400">
-  </a>
-  <div class="desc">PHOTOS4</div>
-</div>
-  <div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS5.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS5.jpg" alt="Cinque Terre" width="600" height="400">
-  </a>
-  <div class="desc">PHOTOS5</div>
-</div>
-  <div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS6.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS6.jpg" alt="Cinque Terre" width="600" height="400">
-  </a>
-  <div class="desc">PHOTOS6</div>
-</div>
-  <div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS7.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS7.jpg" alt="Cinque Terre" width="600" height="400">
-  </a>
-  <div class="desc">PHOTOS7</div>
-</div>
-  <div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS8.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS8.jpg" alt="Cinque Terre" width="600" height="400">
-  </a>
-  <div class="desc">PHOTOS8</div>
-</div>
-  <div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS9.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS9.jpg" alt="Cinque Terre" width="600" height="400">
-  </a>
-  <div class="desc">AYTH MAS EXEI DOSEI E? </div>
-</div>
-  <div class="gallery">
-  <a target="_blank" href="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS10.jpg">
-    <img src="https://baby001iscoming.blob.core.windows.net/mamasboy/PHOTOS10.jpg" alt="Cinque Terre" width="600" height="400">
-  </a>
-  <div class="desc">PHOTOS10</div>
+  <div class="desc">$i</div>
+  ?>
 </body>
 </html> 
