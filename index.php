eb8bdc1d51a7ee2e3cc175b3e7c4d701c1adef4b
toLearnAz <114563126+toLearnAz@users.noreply.github.com>
2023-05-23 11:56:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
-class IndexController extends Yaf_Controller_Abstract {
-   /* default action */
-   public function indexAction() {
-       $this->_view->word = "hello world";
-       //or
-       // $this->getView()->word = "hello world";
-   }
-}
+  $this->getView()->word = "hello world";
 ?>
-
-
-
 
 <html>
 <head>
